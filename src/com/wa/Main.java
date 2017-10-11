04976a52c15a86088c50fc5aafca5fd3b61105d6
lev.romanets <lev.romanets@worldapp.com>
2017-10-11 22:57:57 +0300

    2600

--- src/com/wa/Main.java
+++ src/com/wa/Main.java
@@ -3,6 +3,8 @@ package com.wa;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int n = 2600;
+        String st = "Hello";
+        System.out.println(st + " " + n);
     }
 }
