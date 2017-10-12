1029cb4e369c029d04693e4a65a20f7f75bf8e48
lev.romanets <lev.romanets@worldapp.com>
2017-10-12 09:49:16 +0300

    rename class

--- src/com/wa/Main.java
+++
@@ -1,8 +0,0 @@
-package com.wa;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/com/wa/Test.java
@@ -0,0 +1,27 @@
+package com.wa;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input time");
+        int time = sc.nextInt();
+        System.out.println("Input money");
+        double money = sc.nextDouble();
+        System.out.println("Input name");
+        String human = sc.next();
+        System.out.println("Input energy");
+        boolean energy = sc.hasNextBoolean();
+
+        boolean timeR = time < 24 && time > 8;
+        boolean moneyR = money > 100;
+
+        boolean finR = timeR && moneyR && energy;
+
+        System.out.println(finR);
+    }
+
+}
