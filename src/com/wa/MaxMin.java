d5505c6e1040d0081c993eee129369dedda7451e
lev.romanets <lev.romanets@worldapp.com>
2017-10-12 10:50:58 +0300

    WorkTime added

---
+++ src/com/wa/MaxMin.java
@@ -0,0 +1,48 @@
+package com.wa;
+
+import java.util.Scanner;
+
+public class MaxMin {
+    public static void main(String[] args) {
+
+        Scanner n = new Scanner(System.in);
+        int max;
+        int min;
+
+        System.out.println("Input 1st number");
+        int one = n.nextInt();
+        System.out.println("Input 2nd number");
+        int two = n.nextInt();
+        System.out.println("Input 3rd number");
+        int three = n.nextInt();
+
+        if (one > two){
+            max = one;
+            if (max < three){
+                max = three;
+            }
+        }
+        else {
+            max = two;
+            if (max < three) {
+                max = three;
+            }
+        }
+
+        if (one > two){
+            min = two;
+            if (min > three){
+                min = three;
+            }
+        }
+        else {
+            min = one;
+            if (min > three) {
+                min = three;
+            }
+        }
+
+        System.out.println("Max = " + max);
+        System.out.println("Min = " + min);
+    }
+}
